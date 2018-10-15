e968e24cfda50d7c7b6f97fc4b75b25fdce91a36
Denis <dbalagov@holbi.co.uk>
2018-10-15 10:34:53 +0300

    refactor

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -23,43 +23,43 @@ class UserChecker implements UserCheckerInterface
      * {@inheritdoc}
      */
     public function checkPreAuth(UserInterface $user)
-{
-    if (!$user instanceof User) {
-        return;
-    }
+    {
+        if (!$user instanceof User) {
+            return;
+        }
 
-    if (!$user->isAccountNonLocked()) {
-        $ex = new LockedException('User account is locked.');
-        $ex->setUser($user);
-        throw $ex;
-    }
+        if (!$user->isAccountNonLocked()) {
+            $ex = new LockedException('User account is locked.');
+            $ex->setUser($user);
+            throw $ex;
+        }
 
-    if (!$user->isEnabled()) {
-        $ex = new DisabledException('User account is disabled.');
-        $ex->setUser($user);
-        throw $ex;
-    }
+        if (!$user->isEnabled()) {
+            $ex = new DisabledException('User account is disabled.');
+            $ex->setUser($user);
+            throw $ex;
+        }
 
-    if (!$user->isAccountNonExpired()) {
-        $ex = new AccountExpiredException('User account has expired.');
-        $ex->setUser($user);
-        throw $ex;
+        if (!$user->isAccountNonExpired()) {
+            $ex = new AccountExpiredException('User account has expired.');
+            $ex->setUser($user);
+            throw $ex;
+        }
     }
-}
 
     /**
      * {@inheritdoc}
      */
     public function checkPostAuth(UserInterface $user)
-{
-    if (!$user instanceof AdvancedUserInterface) {
-        return;
-    }
+    {
+        if (!$user instanceof User) {
+            return;
+        }
 
-    if (!$user->isCredentialsNonExpired()) {
-        $ex = new CredentialsExpiredException('User credentials have expired.');
-        $ex->setUser($user);
-        throw $ex;
+        if (!$user->isCredentialsNonExpired()) {
+            $ex = new CredentialsExpiredException('User credentials have expired.');
+            $ex->setUser($user);
+            throw $ex;
+        }
     }
-}
 }
